dcef42cbba30d0006551e7c1d2ee7d2ded2203d5
Imre Patrik <150800007+ImrePatrik@users.noreply.github.com>
2025-12-25 02:43:54 +0100

    Update sw-topics.js

--- sw-topics.js
+++ sw-topics.js
@@ -1,21 +1,11 @@
-const CACHE_NAME = 'floor-topics-v1';
-const urlsToCache = [
-  '/The-Floor-Topics/index.html',
-  '/The-Floor-Topics/manifest-topics.json',
-  '/The-Floor-Topics/icon-192.png',
-  '/The-Floor-Topics/icon-512.png'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
