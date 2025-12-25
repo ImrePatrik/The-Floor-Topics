a7374834fcd87bb96add95033648c545ae4e841e
Imre Patrik <150800007+ImrePatrik@users.noreply.github.com>
2025-12-25 21:04:35 +0100

    Update sw-topics.js

--- sw-topics.js
+++ sw-topics.js
@@ -1,9 +1,15 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => caches.delete(cache))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
